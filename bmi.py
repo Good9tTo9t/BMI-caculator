3c9715ae01d34fa8d3459449521973874bfcb0dc
HiroOnGitHub <49119877+HiroOnGithub@users.noreply.github.com>
2020-05-11 10:32:11 +0800

    Simplified Everything
    Executable will be updated later.

--- bmi.py
+++ bmi.py
@@ -5,17 +5,16 @@ heightCM = input("請輸入身高(公分): ")
 heightCM = float(heightCM)
 print("計算中...")
 heightM = float(heightCM / 100)
-heightSquared = heightM ** 2
-BMI = weight // heightSquared
-BMIStr = str(BMI)
+heightSquare = heightM ** 2
+BMI = weight / heightSquared
 
 if BMI < 19:
-    print("BMI為" + BMIStr + "，太瘦了啦")
+    print("BMI為%f，太瘦了啦"%(BMI))
 elif BMI > 19 and BMI < 24:
-    print("BMI為" + BMIStr + "，正常的啦")
+    print("BMI為%f，正常的啦"%(BMI)")
 elif BMI > 24 and BMI < 28:
-    print("BMI為" + BMIStr + "，有點胖啦")
+    print("BMI為%f，有點胖啦"%(BMI)")
 elif BMI >= 28:
-    print("BMI為" + BMIStr + "，太胖了啦")
+    print("BMI為%f，太胖了啦"%(BMI)")
 input("按enter離開:")
 exit()
