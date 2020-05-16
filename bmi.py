9365dd6351f7c23c0739622b3fecd1a8a88f65d0
HiroOnGitHub <49119877+HiroOnGithub@users.noreply.github.com>
2020-05-16 20:02:46 +0800

    Simplified Everything

--- bmi.py
+++ bmi.py
@@ -9,10 +9,10 @@ BMI = weight / heightSquared
 if BMI < 19:
     print("BMI為%f，太瘦了啦"%(BMI))
 elif BMI > 19 and BMI < 24:
-    print("BMI為%f，正常的啦"%(BMI)")
+    print("BMI為%f，正常的啦"%(BMI))
 elif BMI > 24 and BMI < 28:
-    print("BMI為%f，有點胖啦"%(BMI)")
+    print("BMI為%f，有點胖啦"%(BMI))
 elif BMI >= 28:
-    print("BMI為%f，太胖了啦"%(BMI)")
+    print("BMI為%f，太胖了啦"%(BMI))
 input("按enter離開:")
 exit()
